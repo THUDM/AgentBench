e1c668bc13b1de42558a6d37244fffbf0f11253a
Yixing <108294040+jyx-su@users.noreply.github.com>
2024-12-10 17:21:26 -0800

    Update start_fhir.sh
    Include first-time setup

--- MedAgentBench/start_fhir.sh
+++ MedAgentBench/start_fhir.sh
@@ -1,5 +1,9 @@
-cd MedAgentBench-dev
+#First-time setup
+sudo usermod -aG docker $USER
+docker pull hapiproject/hapi:latest
 
-docker run -d -p 8080:8080 -v $(pwd)/:/configs -e "--spring.config.location=file:///configs/application.yaml" -v /home/yx/my_h2_data:/data  hapiproject/hapi:latest
+
+cd MedAgentBench-dev
+docker run -p 8080:8080 -v $(pwd)/:/configs -e "--spring.config.location=file:///configs/application.yaml" -v /home/yx/my_h2_data:/data  hapiproject/hapi:latest
 
 
