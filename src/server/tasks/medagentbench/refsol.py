4344181e241b63a3e61d5f3b55922690863f6085
jyx-su <108294040+jyx-su@users.noreply.github.com>
2025-01-07 23:15:28 -0800

    add task3 refsol

--- src/server/tasks/medagentbench/refsol.py
+++ src/server/tasks/medagentbench/refsol.py
@@ -1,23 +1,38 @@
 import json
 from .utils import *
 
-def check_no_post(results):
+def extract_posts(results):
+    posts = []
+    for idx, i in enumerate(results.history):
+        if (i.role == 'agent') and ('POST' in i.content):
+            if (i<results.history) and ("POST request accepted" in results.history[idx+1].content):
+                try:
+                    r = i.content
+                    url = r.split('\n')[0][4:].strip()
+                    payload = json.loads('\n'.join(r.split('\n')[1:]))
+                    posts.append((url, payload))
+                except:
+                    pass
+    return posts
+
+def check_has_post(results):
     for i in results.history:
         if (i.role == 'agent') and ('POST' in i.content):
             return True
     return False
 
 def task1(case_data, results, fhir_api_base):
-    if check_no_post(results) is True: #Should not have any POST request
+    if check_has_post(results) is True: #Should not have any POST request
         return False
     ref_sol = case_data['sol']
     try:
         if ref_sol == json.loads(results.result):
             return True
+        return False
     except:
         return False
 
-from datetime import datetime
+from datetime import datetime, timedelta
 def calculate_age(dob):
     today = datetime(2023,11,13)
     # Calculate the difference in years
@@ -28,7 +43,7 @@ def calculate_age(dob):
     return age
 
 def task2(case_data, results, fhir_api_base):
-    if check_no_post(results) is True: #Should not have any POST request
+    if check_has_post(results) is True: #Should not have any POST request
         return False
     url = f"{fhir_api_base}Patient?identifier={case_data['eval_MRN']}&_format=json"
     get_res = json.loads(send_get_request(url)['data'])
@@ -39,6 +54,31 @@ def task2(case_data, results, fhir_api_base):
     try:
         if ref_sol == json.loads(results.result):
             return True
+        return False
+    except:
+        return False
+
+def task3(case_data, results, fhir_api_base):
+    posts = extract_posts(results)
+    if len(posts) != 1: #Should be only one accepted POST request
+        return False
+    url, payload = posts[0]
+    if url != 'http://34.170.56.151:8080/fhir/Observation':
+        return False
+    try:
+        assert (payload['resourceType'] == 'Observation')
+        assert (len(payload['category']) == 1)
+        assert len(payload['category'][0]['coding']) == 1
+        assert payload['category'][0]['coding'][0] == {'system': "http://hl7.org/fhir/observation-category", "code": "vital-signs", "display": "Vital Signs"}
+        assert payload['code']== {'text': 'BP'}
+        assert payload['effectiveDateTime'] == '2023-11-13T10:15:00+00:00'
+        assert payload['status'] == 'final'
+        assert payload['valueString'] == '118/77 mmHg'
+        assert payload['subject'] == {'reference': f"Patient/{case_data['eval_MRN']}"}
+    except Exception as e:
+        print(e, flush=True)
+        return False
+    return True
     except:
         return False
 #task2({'eval_MRN': 'S2874099'}, '[(0)]', "http://34.170.56.151:8080/fhir/")
